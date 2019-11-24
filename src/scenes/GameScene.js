13784a3d3f8f2a5e5f331f90a27434fd89c69f41
Luca Cattide <l.cattide@email.it>
2019-11-24 18:30:42 +0100

    Setup

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,48 @@
+// Module Start
+// Game
+// JS imports
+import 'phaser';
+
+/**
+ * @description Scene
+ * @author Luca Cattide
+ * @date 2019-07-02
+ * @export
+ * @class GameScene
+ * @extends {Phaser.Scene}
+ */
+export default class GameScene extends Phaser.Scene {
+  /**
+   * Creates an instance of GameScene.
+   * @author Luca Cattide
+   * @date 2019-07-02
+   * @memberof GameScene
+   */
+  constructor() {
+    super('Game');
+  }
+
+  /**
+   * @description Assets preloading
+   * @author Luca Cattide
+   * @date 2019-07-02
+   * @memberof GameScene
+   */
+  preload() {}
+
+  /**
+   * @description Assets creation
+   * @author Luca Cattide
+   * @date 2019-07-02
+   * @memberof GameScene
+   */
+  create() {}
+
+  /**
+   * @description Scene updating
+   * @author Luca Cattide
+   * @date 2019-07-02
+   * @memberof GameScene
+   */
+  update() {}
+};
